46e5b5578ba04d51508ec816fb41c39cb5496995
Tyler Watson <tyler@tw.id.au>
2022-08-18 20:58:58 +1000

    dup: add garbage collection
    gun_dup_context now has a sul member and a pointer to the websocket
    context to schedule a cleanup of expired dup entries every 60 seconds.

--- src/dup.c
+++ src/dup.c
@@ -2,23 +2,61 @@
 //  SPDX-FileCopyrightText: 2022 Tyler Watson <tyler@tw.id.au>
 //  SPDX-License-Identifier: MIT
 
+#include <time.h>
+
 #include "gun.h"
 #include "dup.h"
 #include "ht.h"
 #include "log.h"
-#include <time.h>
 
-int gun_dup_init(struct gun_dup_context *context, unsigned ttl)
+#define GARBAGE_TIMER_USECS 60 * 1000 * 1000
+
+void __gun_dup_collect_garbage(struct lws_sorted_usec_list *sul)
+{
+	struct gun_dup_context *context =
+		lws_container_of(sul, struct gun_dup_context, sul);
+	struct ht_iterator iter = ht_iterator(context->id_table);
+	struct gun_dup_entry *entry;
+	time_t now;
+	int count = 0;
+
+	while (ht_next(&iter)) {
+		entry = (struct gun_dup_entry *)iter.value;
+		time(&now);
+
+		if (entry == NULL) {
+			continue;
+		}
+
+		if (difftime(entry->expiry, now) < 0) {
+			ht_remove(context->id_table, iter.key);
+			free(entry);
+			count++;
+		}
+	}
+
+	log_trace("dup: gc: %d expired items removed");
+
+	lws_sul_schedule(context->ws_context, 0, &context->sul,
+			 __gun_dup_collect_garbage, GARBAGE_TIMER_USECS);
+}
+
+int gun_dup_init(const struct gun_context *gun_context,
+		 struct gun_dup_context *context, unsigned ttl)
 {
 	if ((context->id_table = ht_create()) == NULL) {
-		log_fatal("dup: cannot allocate hashtable; out of memory.\n");
+		log_fatal("dup: cannot allocate hashtable; out of memory.");
 		return -ENOMEM;
 	}
 
-	log_debug("dup: init hashtable with %d entries and %d ttl\n",
+	log_debug("dup: init hashtable with %d entries and %d ttl",
 		  context->id_table->capacity, ttl);
 
 	context->ttl = ttl;
+	context->ws_context = gun_context->ws_context;
+
+	lws_sul_schedule(context->ws_context, 0, &context->sul,
+			 __gun_dup_collect_garbage, GARBAGE_TIMER_USECS);
 
 	return 0;
 }
@@ -37,7 +75,7 @@ int gun_dup_track(struct gun_dup_context *context, const char *id)
 			log_debug(
 				"dup: %s: seen it before but it expired %d secs ago,"
 				"entry is trash and we will clean it up and act like "
-				"we've never seen it.\n",
+				"we've never seen it.",
 				id, age * -1);
 			entry = (struct gun_dup_entry *)ht_remove(
 				context->id_table, id);
@@ -51,7 +89,7 @@ int gun_dup_track(struct gun_dup_context *context, const char *id)
 	if (!entry) {
 		log_debug("dup: %s: not seen this ID before", id);
 		if ((entry = malloc(sizeof(struct gun_dup_entry))) == NULL) {
-			log_fatal("dup: out of memory allocating dup entry\n");
+			log_fatal("dup: out of memory allocating dup entry");
 			return -ENOMEM;
 		}
 		ht_set(context->id_table, id, entry);
